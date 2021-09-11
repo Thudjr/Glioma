4e1d72e079fda09a344184b6ff9b0d12e974b4e3
Thudjr <50898332+Thudjr@users.noreply.github.com>
2021-09-11 18:37:39 +0800

    Create user_define.py

---
+++ user_define.py
@@ -0,0 +1,44 @@
+import os
+
+
+
+
+
+
+def hyperParameters():
+  '''
+  根据数据自定义
+  '''
+  glioma_types = {
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
